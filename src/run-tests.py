3f0b3f808bb81094e54a4b38c36a44ce1cea322d
Ian Briggs <ianbriggsutah@gmail.com>
2021-09-27 10:51:56 -0600

    switch over to ch5

--- src/run-tests.py
+++ src/run-tests.py
@@ -73,7 +73,7 @@ def parse_arguments(argv):
     parser = argparse.ArgumentParser(description='WBE test runner')
     parser.add_argument("chapter",
                         nargs="?",
-                        default="chapter4",
+                        default="chapter5",
                         choices=list(CURRENT_TESTS),
                         help="Which chapter's tests to run")
     args = parser.parse_args(argv[1:])
